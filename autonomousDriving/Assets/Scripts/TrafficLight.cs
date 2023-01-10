c7a3cf0e91e9f69f14f4db26494b593f17a48eb4
mjmj1 <mjong3365@gmail.com>
2023-01-10 10:35:04 +0900

    update

--- autonomousDriving/Assets/Scripts/TrafficLight.cs
+++ autonomousDriving/Assets/Scripts/TrafficLight.cs
@@ -18,34 +18,31 @@ public class TrafficLight : MonoBehaviour
 
     public float count = 10f;
 
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
     void Update()
     {
         if(count < 0)
         {
-            Yellow.SetActive(true);
             Red.SetActive(false);
+            Yellow.SetActive(true);
             Green.SetActive(true);
             DeathLine.SetActive(true);
+
             count = Random.Range(20, 30);
         }
         else if (count < 3)
         {
             Green.SetActive(true);
             Yellow.SetActive(false);
+
             count -= Time.deltaTime;
         }
         else if(count < 12)
         {
-            Green.SetActive(false);
-            Yellow.SetActive(true);
             Red.SetActive(true);
+            Yellow.SetActive(true);
+            Green.SetActive(false);
             DeathLine.SetActive(false);
+
             count -= Time.deltaTime;
         }
         else
